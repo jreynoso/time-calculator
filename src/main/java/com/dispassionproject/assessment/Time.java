7c40ead586361e04da44de8fe4f4d2771db46b08
jreynoso <jreynoso@gmail.com>
2020-04-23 23:56:13 -0500

    BLUE: moved static methods from Time to TimeCalculator

--- src/main/java/com/dispassionproject/assessment/Time.java
+++ src/main/java/com/dispassionproject/assessment/Time.java
@@ -9,9 +9,8 @@ import lombok.Getter;
 @Getter
 class Time {
 
-    private static final String TIME_FORMAT = "^(1[0-2]|[1-9]):[0-5][0-9] (AM|PM)$";
-    private static final String AM = "AM";
-    private static final String PM = "PM";
+    static final String AM = "AM";
+    static final String PM = "PM";
 
     private int hours;
     private int minutes;
@@ -44,34 +43,6 @@ class Time {
     String to12HourFormat() {
         final int convertedHours = hours == 0 ? 12 : hours <= 12 ? hours : hours - 12;
         return String.format("%d:%02d %s", convertedHours, minutes, hours < 12 ? AM : PM);
-    }
-
-    static Time fromFormattedTime(final String formatted12HourTime) {
-        if (!isValid12HourTime(formatted12HourTime)) {
-            return null;
-        }
-
-        int hours = getHours(formatted12HourTime);
-        int minutes = getMinutes(formatted12HourTime);
-        return Time.builder()
-                .hours(hours)
-                .minutes(minutes)
-                .build();
-    }
-
-    private static int getHours(final String formatted12HourTime) {
-        final boolean isAm = formatted12HourTime.endsWith(AM);
-        final int hours = Integer.parseInt(formatted12HourTime.split(":", 2)[0]);
-        final int offset = isAm ? 0 : 12;
-        return hours == 12 ? (isAm ? 0 : 12) : hours + offset;
-    }
-
-    private static int getMinutes(final String formatted12HourTime) {
-        return Integer.parseInt(formatted12HourTime.split("[:|\\s]", 3)[1]);
-    }
-
-    public static boolean isValid12HourTime(final String formatted12HourTime) {
-        return formatted12HourTime != null && formatted12HourTime.matches(TIME_FORMAT);
     }
 
 }
--- src/main/java/com/dispassionproject/assessment/TimeCalculator.java
+++ src/main/java/com/dispassionproject/assessment/TimeCalculator.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TimeCalculator {
 
+    private static final String TIME_FORMAT = "^(1[0-2]|[1-9]):[0-5][0-9] (AM|PM)$";
+
     public static String addMinutes(final String formattedTime, final int offsetMinutes) {
-        final Time time = Time.fromFormattedTime(formattedTime);
+        final Time time = fromFormattedTime(formattedTime);
         if (time == null) {
             throw new IllegalArgumentException(String.format("%s is not a valid time input.", formattedTime));
         }
@@ -19,6 +21,34 @@ public class TimeCalculator {
         time.adjustMinutes(minutesOffset);
 
         return time.to12HourFormat();
+    }
+
+    private static Time fromFormattedTime(final String formatted12HourTime) {
+        if (!isValid12HourTime(formatted12HourTime)) {
+            return null;
+        }
+
+        int hours = getHours(formatted12HourTime);
+        int minutes = getMinutes(formatted12HourTime);
+        return Time.builder()
+                .hours(hours)
+                .minutes(minutes)
+                .build();
+    }
+
+    private static int getHours(final String formatted12HourTime) {
+        final boolean isAm = formatted12HourTime.endsWith(Time.AM);
+        final int hours = Integer.parseInt(formatted12HourTime.split(":", 2)[0]);
+        final int offset = isAm ? 0 : 12;
+        return hours == 12 ? (isAm ? 0 : 12) : hours + offset;
+    }
+
+    private static int getMinutes(final String formatted12HourTime) {
+        return Integer.parseInt(formatted12HourTime.split("[:|\\s]", 3)[1]);
+    }
+
+    private static boolean isValid12HourTime(final String formatted12HourTime) {
+        return formatted12HourTime != null && formatted12HourTime.matches(TIME_FORMAT);
     }
 
 }
